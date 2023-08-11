4a37fd847777bb179a353b188ec5e3756c628b09
aafaquerk <arkhan@email.arizona.edu>
2023-08-10 18:29:02 -0700

    Update conf.py
    Updating project information

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'EMCCD_QE_Analysis'
+copyright = '2023, Aafaque R Khan'
+author = 'Aafaque R Khan'
 
 release = '0.1'
-version = '0.1.0'
+version = '0.0.2'
 
 # -- General configuration
 
